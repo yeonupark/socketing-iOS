58beadbe2a04d3c7ca8050938a3c868570b3e58f
yeonupark <idepix@naver.com>
2024-12-23 22:56:59 +0900

    chore: Initialize project

---
+++ Socketing/Socketing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Socketing
+//
+//  Created by Yeonu Park on 2024/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
