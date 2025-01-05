57c86d7ce2bd8002615fbaa58ab1170236ad9af1
yeonupark <idepix@naver.com>
2025-01-05 16:14:39 +0900

    feat: Implement RxSwift-based data flow for MainViewController

--- Socketing/Socketing/Controllers/MainViewController.swift
+++ Socketing/Socketing/Controllers/MainViewController.swift
@@ -6,12 +6,35 @@
 //
 
 import UIKit
+import RxSwift
 
 class MainViewController: UIViewController {
+    
+    private let mainView = MainView()
+    private let viewModel = MainViewModel()
+    private let disposeBag = DisposeBag()
+    
+    override func loadView() {
+        view.self = mainView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        bind()
+        viewModel.fetchEvents()
+    }
+    
+    private func bind() {
+        viewModel.events
+            .bind(to: mainView.tableView.rx.items(cellIdentifier: "EventsTableViewCell", cellType: EventsTableViewCell.self)) { (_, element, cell) in
+//                cell.thumbnail.image. = element.thumbnail
+                print(element)
+                cell.titleLabel.text = element.title
+                cell.dateLabel.text = element.eventDates[0].date
+                cell.placeLabel.text = element.place
+            }
+            .disposed(by: disposeBag)
     }
 
 
--- Socketing/Socketing/ViewModels/MainViewModel.swift
+++ Socketing/Socketing/ViewModels/MainViewModel.swift
@@ -11,4 +11,34 @@ import RxCocoa
 
 class MainViewModel {
     
+    let events = BehaviorRelay<[EventData]>(value: [])
+    
+    private let disposeBag = DisposeBag()
+
+    func fetchEvents() {
+        print("fetch events")
+        getEvents()
+            .subscribe(onNext: { [weak self] newEvents in
+                print(newEvents)
+                self?.events.accept(newEvents)
+            }, onError: { error in
+                print("Error fetching events: \(error)")
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func getEvents() -> Observable<[EventData]> {
+        print("get events")
+        // 네트워크 요청 수행
+        return Observable.create { observer in
+            // Mock 데이터
+            let mockData = [
+                EventData(id: "1", title: "투바투 콘서트", eventDates: [EventDate(id: "00", date: "2025.02.12")], thumbnail: "test", place: "상암경기장", cast: "투모로우바이투게더", ticketingStartTime: "2024.12.31"),
+                EventData(id: "2", title: "뉴진스 콘서트", eventDates: [EventDate(id: "00", date: "2025.03.03")], thumbnail: "test", place: "올림픽주경기장", cast: "newJeans", ticketingStartTime: "2024.12.31")
+            ]
+            observer.onNext(mockData)
+            observer.onCompleted()
+            return Disposables.create()
+        }
+    }
 }
