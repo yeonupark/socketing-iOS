89e5f000db5ce31350eeae454dd22d3234ea2a4f
yeonupark <idepix@naver.com>
2025-01-30 04:07:37 +0900

    feat: Add MyPageVc

---
+++ Socketing/Socketing/Controllers/MyPageViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MyPageViewController.swift
+//  Socketing
+//
+//  Created by Yeonu Park on 2025/01/29.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+    
+    let mainView = MyPageView()
+    let viewModel = MyPageViewModel()
+    
+    override func loadView() {
+        view.self = mainView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainView.tableView.delegate = self
+        mainView.tableView.dataSource = self
+        
+        configureNavigationBar()
+        
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.title = "마이페이지"
+    }
+    
+}
+
+extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = mainView.tableView.dequeueReusableCell(withIdentifier: "BasicCell", for: indexPath)
+        cell.textLabel?.text = viewModel.data[indexPath.row]
+        cell.textLabel?.font = .boldSystemFont(ofSize: 15)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 1 {
+            UserDefaults.standard.removeObject(forKey: "authToken")
+            UserDefaults.standard.removeObject(forKey: "entranceToken")
+            let vc = LoginViewController()
+            self.navigationController?.setViewControllers([vc], animated: true)
+        }
+        mainView.tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
---
+++ Socketing/Socketing/ViewModels/MyPageViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MyPageViewModel.swift
+//  Socketing
+//
+//  Created by Yeonu Park on 2025/01/29.
+//
+
+import Foundation
+
+class MyPageViewModel {
+    
+    let data = ["내가 예매한 공연 목록", "로그아웃"]
+    
+}
