110efebec4fe8850c223a7ccf5713c4327c5bb4c
yeonupark <idepix@naver.com>
2025-02-04 02:32:27 +0900

    feat: Add alert when no friend registered while together-booking

--- Socketing/Socketing/Controllers/EventDetailViewController.swift
+++ Socketing/Socketing/Controllers/EventDetailViewController.swift
@@ -60,6 +60,10 @@ class EventDetailViewController: BaseViewController {
         mainView.togetherBookingButton.rx.tap
             .asDriver()
             .drive(onNext: { [weak self] in
+                if self?.viewModel.numberOfFriends.value == 0 {
+                    self?.togetherButtonDisabled()
+                    return
+                }
                 UserDefaults.standard.setValue(true, forKey: "enter")
                 let vc = WaitingViewController()
                 self?.navigationController?.pushViewController(vc, animated: true)
@@ -71,31 +75,38 @@ class EventDetailViewController: BaseViewController {
     }
     
     @objc func showNameInputModal() {
-            let alert = UIAlertController(title: "함께할 친구 등록하기",
-                                           message: "친구의 이름을 입력해주세요",
-                                           preferredStyle: .alert)
-            
-            alert.addTextField { textField in
-                textField.placeholder = "이름을 입력하세요"
-            }
+        let alert = UIAlertController(title: "함께할 친구 등록하기",
+                                      message: "친구의 이름을 입력해주세요",
+                                      preferredStyle: .alert)
+        
+        alert.addTextField { textField in
+            textField.placeholder = "이름을 입력하세요"
+        }
         
         let confirmAction = UIAlertAction(title: "확인", style: .default) { _ in
-                if let name = alert.textFields?.first?.text, !name.isEmpty {
-                    let numberOfTickets = self.viewModel.numberOfFriends.value + 1
-                    self.viewModel.numberOfFriends.accept(numberOfTickets)
-                } else {
-                    print("이름이 입력되지 않았습니다")
-                }
+            if let name = alert.textFields?.first?.text, !name.isEmpty {
+                let numberOfTickets = self.viewModel.numberOfFriends.value + 1
+                self.viewModel.numberOfFriends.accept(numberOfTickets)
+            } else {
+                print("이름이 입력되지 않았습니다")
             }
-            
-            let cancelAction = UIAlertAction(title: "취소", style: .cancel) { _ in
-                print("입력 취소")
-            }
-            
-            alert.addAction(confirmAction)
-            alert.addAction(cancelAction)
-            
-            self.present(alert, animated: true, completion: nil)
         }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel) { _ in
+            print("입력 취소")
+        }
+        
+        alert.addAction(confirmAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func togetherButtonDisabled() {
+        let alert = UIAlertController(title: "함께할 친구를 등록해주세요", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default))
+        self.present(alert, animated: true)
+        
+    }
 
 }
